f06bda5383ba7144e6be322a73405800c56d7796
Jackson Prowell <prowelljackson@gmail.com>
2023-11-05 16:16:29 -0800

    chore(main): run statistical summary instead of CLIP

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ import firestore
 import clip
 import multiprocessing
 import spectogram
-import typesense
 from embeddings import typesense 
 import glob
 import os
 import mp3data
 import gc
+import spectogram_data_embeddings 
 
 def create_spectrogram(artist, title, audio_filepath):
     # Limit the frequency range to 20kHz
@@ -24,14 +24,6 @@ def generate_embeddings(spectrogram_image_path):
     print("Generating embeddings for spectrogram: " + spectrogram_image_path)
     return clip.get_embeddings(spectrogram_image_path)
 
-def save_embeddings_to_typesense(song_id, embeddings):
-    print("Saving embeddings to Typesense")
-    document = {
-        'song_id': song_id,
-        'embedding': embeddings
-    }
-    typesense.client.collections['songs'].documents.create(document)
-
 def get_song_paths(root_dir="data/songs"):
     """
     Retrieves a list of all song paths within the given root directory.
@@ -42,13 +34,9 @@ def get_song_paths(root_dir="data/songs"):
     # Read the data/songs directory and get the list of files (nested in subdirectories)
     song_paths = glob.glob(os.path.join(root_dir, '**', '*.mp3'), recursive=True)
     return song_paths
-    
-def process_song(song_path):
-    print("\n=========================================\n")
-    artist, title = mp3data.get_id3_tag_info(song_path)
 
+def embed_with_CLIP(artist, title, song_path):
     # Verify the song hasn't already been added
-
     song_exists, song_id = firestore.check_song_exists(title, artist)
     if song_exists: 
         print(f"Song {title} by {artist} already exists in Firestore.")
@@ -60,8 +48,35 @@ def process_song(song_path):
         return
     docId = save_to_firestore(title, artist, song_path)
     embeddings = generate_embeddings(spectrogram_image_path)
-    save_embeddings_to_typesense(docId, embeddings)
+    typesense.save_embeddings_to_typesense(docId, embeddings)
     gc.collect()
+
+def embed_with_stat_summary(artist, title, song_path):
+    try:
+        song_exists, doc_id= firestore.check_song_exists(title, artist)
+        if song_exists:
+            print(f"Song {title} by {artist} already exists in Firestore.")
+            return
+
+        print(f"Generating embedding for {title} by {artist}")
+        embedding = spectogram_data_embeddings.get_embedding(song_path) 
+        doc_id = firestore.add_song(title, artist, song_path)
+        typesense.save_embeddings_to_typesense(doc_id, embedding.tolist(), song_path)
+        gc.collect()
+    except Exception as e:
+        print(f"Error embedding song {title} by {artist}: {e}")
+
+    
+def process_song(song_path):
+    print("\n=========================================\n")
+    artist, title = mp3data.get_id3_tag_info(song_path)
+
+    # For CLIP embedding:
+    # embed_with_CLIP(artist, title, song_path)
+
+    # For Statistical Summary embedding:
+    embed_with_stat_summary(artist, title, song_path)
+
 
 # first attempt: Command line callable with arguments for artist, title, and audiofile path
 if __name__ == "__main__":
